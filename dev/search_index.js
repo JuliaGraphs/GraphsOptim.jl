f2308bb7b74431fab27f471d9b4dbd57a61b3cc8
Documenter.jl <documenter@juliadocs.github.io>
2022-03-09 12:20:24 +0000

    build based on 38600e8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = GraphsOptim","category":"page"},{"location":"#GraphsOptim","page":"Home","title":"GraphsOptim","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for GraphsOptim.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [GraphsOptim]","category":"page"}]
+}
