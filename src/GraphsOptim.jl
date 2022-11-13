7b8c579cef5750f54c13cdfafef32eaaa9a833e3
aurorarossi <aurora98rossi@gmail.com>
2022-11-13 21:41:26 +0100

    Add FAQalgorithm.jl

--- src/GraphsOptim.jl
+++ src/GraphsOptim.jl
@@ -15,8 +15,10 @@ using SparseArrays
 include("utils.jl")
 include("minimum_cost_flow.jl")
 include("maximum_weight_matching.jl")
+include("FAQalgorithm.jl")
 
 export minimum_cost_flow
 export maximum_weight_matching, maximum_weight_maximal_matching
+export faq
 
 end
