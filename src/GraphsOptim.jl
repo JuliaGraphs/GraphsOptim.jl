32c0e9ea111b6318d8205996725724a0f136fa93
aurorarossi <aurora98rossi@gmail.com>
2022-11-13 21:52:56 +0100

    Add LinearAlgebra and export more functions

--- src/GraphsOptim.jl
+++ src/GraphsOptim.jl
@@ -11,6 +11,7 @@ using Graphs
 using JuMP
 using SimpleTraits
 using SparseArrays
+using LinearAlgebra
 
 include("utils.jl")
 include("minimum_cost_flow.jl")
@@ -19,6 +20,6 @@ include("FAQalgorithm.jl")
 
 export minimum_cost_flow
 export maximum_weight_matching, maximum_weight_maximal_matching
-export faq
+export faq, _flat_doubly_stochastic, _gradient, _distance, _solve_transportation_problem,_solve_assignment_problem, _step_size, _update_P
 
 end
