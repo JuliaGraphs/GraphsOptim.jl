87733d3955d6df250158fe2b5589451abfe72a4b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-06-28 12:25:01 +0200

    Add JET to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,18 +3,23 @@ using Documenter
 using Graphs
 using GraphsOptim
 using HiGHS
+using JET
 using JuliaFormatter
 using LinearAlgebra
 using SparseArrays
 using Test
 
 @testset verbose = true "GraphsOptim.jl" begin
-    @testset verbose = true "Code quality (Aqua.jl)" begin
+    @testset verbose = true "Quality (Aqua.jl)" begin
         Aqua.test_all(GraphsOptim; ambiguities=false)
     end
 
-    @testset verbose = true "Code formatting (JuliaFormatter.jl)" begin
+    @testset verbose = true "Formatting (JuliaFormatter.jl)" begin
         @test format(GraphsOptim; verbose=false, overwrite=false)
+    end
+
+    @testset verbose = true "Correctness (JET.jl)" begin
+        JET.test_package(GraphsOptim; target_defined_modules=true)
     end
 
     @testset verbose = true "Doctests (Documenter.jl)" begin
