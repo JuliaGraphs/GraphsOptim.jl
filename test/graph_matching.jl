56598e6d847159aec6c208d28bd421a11f4b08ce
aurorarossi <aurora98rossi@gmail.com>
2023-07-12 11:36:33 +0200

    Add tests

--- test/graph_matching.jl
+++ test/graph_matching.jl
@@ -3,20 +3,45 @@ using GraphsOptim
 using LinearAlgebra
 using Test
 
-@test GraphsOptim.is_doubly_stochastic(first(faq(rand(5, 5), rand(5, 5))))
+@testset "FAQ" begin
+    @test GraphsOptim.is_permutation_matrix(first(faq(rand(5, 5), rand(5, 5))))
 
-@testset "Empty graphs" begin
-    A = adjacency_matrix(SimpleGraph(5))
-    B = adjacency_matrix(SimpleGraph(5))
-    _, dist, converged = faq(A, B)
-    @test dist ≈ 0.0
-    @test converged
+    @testset "Empty graphs" begin
+        A = adjacency_matrix(SimpleGraph(5))
+        B = adjacency_matrix(SimpleGraph(5))
+        _, dist, converged = faq(A, B)
+        @test dist ≈ 0.0
+        @test converged
+    end
+
+    @testset "Path and complete graph" begin
+        A = adjacency_matrix(path_graph(3))
+        B = adjacency_matrix(complete_graph(3))
+        _, dist, converged = faq(A, B)
+        @test dist ≈ sqrt(2)
+        @test converged
+    end
+
 end
 
-@testset "Path and complete graph" begin
-    A = adjacency_matrix(path_graph(3))
-    B = adjacency_matrix(complete_graph(3))
-    _, dist, converged = faq(A, B)
-    @test dist ≈ sqrt(2)
-    @test converged
+@testset "GOAT" begin
+    @test GraphsOptim.is_permutation_matrix(first(goat(rand(5, 5), rand(5, 5))))
+
+    @testset "Empty graphs" begin
+        A = adjacency_matrix(SimpleGraph(5))
+        B = adjacency_matrix(SimpleGraph(5))
+        _, dist, converged = goat(A, B)
+        @test dist ≈ 0.0
+        @test converged
+    end
+
+
+    @testset "Path and complete graph" begin
+        A = adjacency_matrix(path_graph(3))
+        B = adjacency_matrix(complete_graph(3))
+        _, dist, converged = goat(A, B)
+        @test dist ≈ sqrt(2)
+        @test converged
+    end
+
 end
