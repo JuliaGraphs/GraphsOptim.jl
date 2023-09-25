e583b9a8fba6c2ca5d7d4c92dea399007d5c9380
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-25 13:54:40 +0200

    Transfer from gdalle and update docs

--- docs/make.jl
+++ docs/make.jl
@@ -3,18 +3,38 @@ using Documenter
 
 DocMeta.setdocmeta!(GraphsOptim, :DocTestSetup, :(using GraphsOptim); recursive=true)
 
+generated_path = joinpath(@__DIR__, "src")
+base_url = "https://github.com/JuliaGraphs/GraphsOptim.jl/blob/main/"
+isdir(generated_path) || mkdir(generated_path)
+
+open(joinpath(generated_path, "index.md"), "w") do io
+    # Point to source license file
+    println(
+        io,
+        """
+        ```@meta
+        EditURL = "$(base_url)README.md"
+        ```
+        """,
+    )
+    # Write the contents out below the meta block
+    for line in eachline(joinpath(dirname(@__DIR__), "README.md"))
+        println(io, line)
+    end
+end
+
 makedocs(;
     modules=[GraphsOptim],
-    authors="Guillaume Dalle <22795598+gdalle@users.noreply.github.com> and contributors",
-    repo="https://github.com/gdalle/GraphsOptim.jl/blob/{commit}{path}#{line}",
+    authors="Guillaume Dalle, Aurora Rossi and contributors",
     sitename="GraphsOptim.jl",
     format=Documenter.HTML(;
+        repolink="https://github.com/JuliaGraphs/GraphsOptim.jl/",
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://gdalle.github.io/GraphsOptim.jl",
+        canonical="https://JuliaGraphs.github.io/GraphsOptim.jl",
         assets=String[],
         edit_link=:commit,
     ),
     pages=["Home" => "index.md", "Algorithms" => "algorithms.md"],
 )
 
-deploydocs(; repo="github.com/gdalle/GraphsOptim.jl", devbranch="main")
+deploydocs(; repo="github.com/JuliaGraphs/GraphsOptim.jl", devbranch="main")
