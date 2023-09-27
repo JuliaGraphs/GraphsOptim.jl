bd6182cf2c2bf12c3538bd6067810080beb14e16
adfeel220 <adfeel220@gmail.com>
2023-09-27 16:12:48 +0200

    Update formatting

--- src/shortest_path.jl
+++ src/shortest_path.jl
@@ -9,38 +9,52 @@ Modify a JuMP model by adding the variable, constraints and objective necessary 
 The edge selection variable will be named `var_name`
 """
 function shortest_path!(
-	model::Model,
-	g::AbstractGraph,
-	source::Int,
-	target::Int,
-	edge_cost::AbstractMatrix;
-	var_name,
+    model::Model,
+    g::AbstractGraph,
+    source::Int,
+    target::Int,
+    edge_cost::AbstractMatrix;
+    var_name,
 )
-	dg = g
-	if !is_directed(g)
-		dg = DiGraph(g)
-	end
-	edge_tuples = [(src(ed), dst(ed)) for ed in edges(dg)]
+    dg = g
+    if !is_directed(g)
+        dg = DiGraph(g)
+    end
+    edge_tuples = [(src(ed), dst(ed)) for ed in edges(dg)]
 
-	# 1 if an edge if traversed and 0 otherwise
-	traversed = @variable(model, 0 <= x[e = edge_tuples] <= 1; integer = true)
-	model[Symbol(var_name)] = traversed
+    # 1 if an edge if traversed and 0 otherwise
+    traversed = @variable(model, 0 <= x[e=edge_tuples] <= 1; integer=true)
+    model[Symbol(var_name)] = traversed
 
-	# Exit from source
-	@constraint(model, sum(traversed[(source, next_v)] for next_v in outneighbors(dg, source)) - sum(traversed[(prev_v, source)] for prev_v in inneighbors(dg, source)) == 1)
-	# Arrive at destination
-	@constraint(model, sum(traversed[(target, next_v)] for next_v in outneighbors(dg, target)) + sum(traversed[(prev_v, target)] for prev_v in inneighbors(dg, target)) == 1)
-	# Go out from a vertex is only possible by going in first
-	for u in vertices(dg)
-		if u in (source, target)
-			continue
-		end
-		@constraint(model, sum(traversed[(u, next_v)] for next_v in outneighbors(dg, u)) - sum(traversed[(prev_v, u)] for prev_v in inneighbors(dg, u)) == 0)
-	end
+    # Exit from source
+    @constraint(
+        model,
+        sum(traversed[(source, next_v)] for next_v in outneighbors(dg, source)) -
+        sum(traversed[(prev_v, source)] for prev_v in inneighbors(dg, source)) == 1
+    )
+    # Arrive at destination
+    @constraint(
+        model,
+        sum(traversed[(target, next_v)] for next_v in outneighbors(dg, target)) +
+        sum(traversed[(prev_v, target)] for prev_v in inneighbors(dg, target)) == 1
+    )
+    # Go out from a vertex is only possible by going in first
+    for u in vertices(dg)
+        if u in (source, target)
+            continue
+        end
+        @constraint(
+            model,
+            sum(traversed[(u, next_v)] for next_v in outneighbors(dg, u)) -
+            sum(traversed[(prev_v, u)] for prev_v in inneighbors(dg, u)) == 0
+        )
+    end
 
-	@objective(model, Min, sum(edge_cost[u, v] * traversed[(u, v)] for (u, v) in edge_tuples))
+    @objective(
+        model, Min, sum(edge_cost[u, v] * traversed[(u, v)] for (u, v) in edge_tuples)
+    )
 
-	return model
+    return model
 end
 
 """
@@ -63,32 +77,31 @@ Returns a sequence of vertices from source to destination.
 - `optimizer`: JuMP-compatible solver (default is `HiGHS.Optimizer`)
 """
 function shortest_path(
-	g::AbstractGraph,
-	source::Int,
-	target::Int,
-	edge_cost::AbstractMatrix;
-	optimizer = HiGHS.Optimizer,
+    g::AbstractGraph,
+    source::Int,
+    target::Int,
+    edge_cost::AbstractMatrix;
+    optimizer=HiGHS.Optimizer,
 )
-	model = Model(optimizer)
-	set_silent(model)
+    model = Model(optimizer)
+    set_silent(model)
 
-	shortest_path!(model, g, source, target, edge_cost; var_name = :path)
-	optimize!(model)
-	@assert termination_status(model) == OPTIMAL
+    shortest_path!(model, g, source, target, edge_cost; var_name=:path)
+    optimize!(model)
+    @assert termination_status(model) == OPTIMAL
 
-	edge_selection = value.(model[:path])
-	valid_edges = [p for p in first(axes(edge_selection)) if edge_selection[p] > 0]
+    edge_selection = value.(model[:path])
+    valid_edges = [p for p in first(axes(edge_selection)) if edge_selection[p] > 0]
 
-	# Parse valid path indices to actual path (sequence of traversed vertices)
-	path = ones(typeof(source), length(valid_edges) + 1)
-	path[begin] = source
+    # Parse valid path indices to actual path (sequence of traversed vertices)
+    path = ones(typeof(source), length(valid_edges) + 1)
+    path[begin] = source
 
-	for step in 1:length(valid_edges)
-		prev = path[step]
-		edge_index = findfirst(edge[begin] == prev for edge in valid_edges)
-		path[step+1] = valid_edges[edge_index][end]
-	end
+    for step in 1:length(valid_edges)
+        prev = path[step]
+        edge_index = findfirst(edge[begin] == prev for edge in valid_edges)
+        path[step + 1] = valid_edges[edge_index][end]
+    end
 
-	return path
+    return path
 end
-
--- test/runtests.jl
+++ test/runtests.jl
@@ -10,37 +10,37 @@ using SparseArrays
 using Test
 
 @testset verbose = true "GraphsOptim.jl" begin
-	@testset "Code formatting" begin
-		@test format(GraphsOptim; verbose = false, overwrite = false)
-	end
+    @testset "Code formatting" begin
+        @test format(GraphsOptim; verbose=false, overwrite=false)
+    end
 
-	if VERSION >= v"1.9"
-		@testset "Code quality" begin
-			Aqua.test_all(GraphsOptim; ambiguities = false)
-		end
+    if VERSION >= v"1.9"
+        @testset "Code quality" begin
+            Aqua.test_all(GraphsOptim; ambiguities=false)
+        end
 
-		@testset "Code linting" begin
-			JET.test_package(GraphsOptim; target_defined_modules = true)
-		end
-	end
+        @testset "Code linting" begin
+            JET.test_package(GraphsOptim; target_defined_modules=true)
+        end
+    end
 
-	@testset "Doctests" begin
-		doctest(GraphsOptim)
-	end
+    @testset "Doctests" begin
+        doctest(GraphsOptim)
+    end
 
-	@testset verbose = true "Flow" begin
-		include("flow.jl")
-	end
+    @testset verbose = true "Flow" begin
+        include("flow.jl")
+    end
 
-	@testset verbose = true "Assignment" begin
-		include("assignment.jl")
-	end
+    @testset verbose = true "Assignment" begin
+        include("assignment.jl")
+    end
 
-	@testset verbose = true "Graph matching" begin
-		include("graph_matching.jl")
-	end
+    @testset verbose = true "Graph matching" begin
+        include("graph_matching.jl")
+    end
 
-	@testset verbose = true "Shortest path" begin
-		include("shortest_path.jl")
-	end
+    @testset verbose = true "Shortest path" begin
+        include("shortest_path.jl")
+    end
 end;
