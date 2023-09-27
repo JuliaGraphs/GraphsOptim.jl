f72afb142133d2e6bb6eaff574b8150985a4f704
adfeel220 <adfeel220@gmail.com>
2023-09-27 16:12:29 +0200

    Fix naming issue

--- test/shortest_path.jl
+++ test/shortest_path.jl
@@ -3,22 +3,22 @@ using GraphsOptim
 using Test
 
 num_vertices = 20
-g = Graphs.SimpleDiGraph(num_vertices, 5 * num_vertices; seed = 0)
+g = Graphs.SimpleDiGraph(num_vertices, 5 * num_vertices; seed=0)
 edge_cost = rand(1:100, num_vertices, num_vertices) .* adjacency_matrix(g)
 
-src = rand(1:num_vertices)
-dst = rand(setdiff(Set(1:num_vertices), Set(src)))
+source = rand(1:num_vertices)
+target = rand(setdiff(Set(1:num_vertices), Set(source)))
 
-dijkstra_parents = Graphs.dijkstra_shortest_paths(g, src, edge_cost).parents
-dijkstra_ans = [dst]
+dijkstra_parents = Graphs.dijkstra_shortest_paths(g, source, edge_cost).parents
+dijkstra_ans = [target]
 while true
-	parent_vx = dijkstra_parents[dijkstra_ans[begin]]
-	pushfirst!(dijkstra_ans, parent_vx)
-	if parent_vx == src
-		break
-	end
+    parent_vx = dijkstra_parents[dijkstra_ans[begin]]
+    pushfirst!(dijkstra_ans, parent_vx)
+    if parent_vx == source
+        break
+    end
 end
 
-mp_ans = GraphsOptim.shortest_path(g, src, dst, edge_cost)
+mp_ans = GraphsOptim.shortest_path(g, source, target, edge_cost)
 
 @test all(dijkstra_ans .== mp_ans)
