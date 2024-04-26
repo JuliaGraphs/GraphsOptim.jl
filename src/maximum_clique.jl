9ec889f06a7ff61561496fa3825580fa02c0391b
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2024-04-26 14:31:16 +0200

    Add to obj

--- src/maximum_clique.jl
+++ src/maximum_clique.jl
@@ -19,7 +19,9 @@ function maximum_weight_clique!(
         packing_constraint[i=1:nv(g), j=1:nv(g); i ≠ j && !has_edge(g, i, j)],
         f[i] + f[j] <= 1,
     )
-    @objective(model, Max, dot(f, vertex_weights))
+    obj = objective_function(model)
+    add_to_expression!(obj, dot(f, vertex_weights))
+    @objective(model, Max, obj)
     return model
 end
 
